d4fc97dbcc45bb2652cd13855115984360e05af7
Hélvio Filho <helviosvf@gmail.com>
2022-07-31 16:54:44 -0300

    Chore: configured styled components with typescript

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,19 @@
+export default {
+  colors: {
+    text: '#0F0F0F',
+    text_mid: '#3E3E3E',
+    text_light: '#CFCFCF',
+
+    background: '#FFFFFF',
+
+    neutral: '#4B92D4',
+
+    success: '#4DCFC0',
+    warning: '#EEA63A',
+    error: '#e52b67',
+  },
+  
+  fonts: {
+
+  },
+}
