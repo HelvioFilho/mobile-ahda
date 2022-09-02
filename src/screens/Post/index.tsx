123380281b02cfef10cd13bcd03088c44a5e38a9
Hélvio Filho <helviosvf@gmail.com>
2022-09-02 17:32:57 -0300

    Feat: created post page

---
+++ src/screens/Post/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+export function Post(){
+  return (
+    <Container>
+      <Text>Post</Text>
+    </Container>
+  );
+}
