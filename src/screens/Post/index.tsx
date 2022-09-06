3291e340ab7a120caed1d510f628fa74f7b30b53
Hélvio Filho <helviosvf@gmail.com>
2022-09-06 14:07:45 -0300

    Feat(Post): showing the posts

--- src/screens/Post/index.tsx
+++ src/screens/Post/index.tsx
@@ -1,12 +1,182 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {
+  ScrollView,
+  StatusBar,
+  useWindowDimensions
+} from 'react-native';
+import RenderHTML from 'react-native-render-html';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { useTheme } from 'styled-components';
+import { ImageGallery } from '../../components/ImageGallery';
+import { api } from '../../services/api';
+import { postStore } from '../../services/store';
 
-import { Container } from './styles';
+import {
+  About,
+  AboutWrapper,
+  Avatar,
+  Container,
+  ContainerAvatar,
+  ContainerGallery,
+  ContainerRender,
+  Cover,
+  CoverWrapper,
+  Footer,
+  Name,
+  Published,
+  Title,
+  TitleGallery
+} from './styles';
 
-export function Post(){
+const { KEY } = process.env;
+
+interface GalleryProps {
+  id: string;
+  path: string;
+}
+
+export function Post() {
+
+  const { data } = postStore();
+  const { width } = useWindowDimensions();
+  const theme = useTheme();
+
+  const date = data.date_post.split(' ');
+  const day = date[0];
+  const hour = date[1];
+
+  let text = data.text.replace(/class="ql-align-center"/g, `style="text-align: center"`);
+  text = text.replace(/class="ql-align-right"/g, `style="text-align: right"`);
+  text = text.replace(/class="ql-align-justify"/g, `style="text-align: justify"`);
+  text = text.replace(/class="ql-size-small"/g, `style="font-size: 0.75rem"`);
+  text = text.replace(/class="ql-size-large"/g, `style="font-size: 1.5rem"`);
+  text = text.replace(/<p><img>/g, `<img`);
+  text = text.replace(/"><\/p>/g, `">`);
+  text = text.replace(/<p><br><\/p>/g, `<p style="margin: 15px 0"></p>`);
+
+  const tagsStyles = {
+    h1: {
+      margin: '5px 20px',
+    },
+    h2: {
+      margin: '0 20px',
+    },
+    h3: {
+      margin: '5px 20px',
+    },
+    h4: {
+      margin: '5px 20px',
+    },
+    h5: {
+      margin: '5px 20px',
+    },
+    h6: {
+      margin: '5px 20px',
+    },
+    img: {
+      margin: '0',
+    },
+    strong: {
+      margin: '0',
+    },
+    p: {
+      margin: '0 20px',
+      fontSize: `${RFValue(15)}px`
+    },
+    ul: {
+      margin: '10px 20px',
+      fontSize: `${RFValue(15)}px`
+    },
+    ol: {
+      margin: '10px 20px',
+      fontSize: `${RFValue(15)}px`
+    }
+  };
+
+  const [imageGallery, setImageGallery] = useState<GalleryProps[]>([]);
+  const [statusBar, setStatusBar] = useState(false);
+
+  async function getImageGallery(id: string) {
+    try {
+      const { data } = await api.get(`get_image_gallery?id=${id}&key=${KEY}`);
+      if (!data.error) {
+        setImageGallery(data.images);
+      } else {
+        console.log(data.error);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  async function handleScroll(event: any) {
+    if (event.nativeEvent.contentOffset.y > 268 && statusBar === false) {
+      setStatusBar(true);
+    }
+    if (event.nativeEvent.contentOffset.y < 268 && statusBar === true) {
+      setStatusBar(false);
+    }
+  }
+
+  useEffect(() => {
+    getImageGallery(data.id);
+  }, []);
+
   return (
-    <Container>
-      <Text>Post</Text>
-    </Container>
+    <>
+      <StatusBar
+        backgroundColor={statusBar ? theme.colors.light : 'transparent'}
+        barStyle={statusBar ? 'dark-content' : 'light-content'}
+        translucent
+      />
+
+      <ScrollView
+        style={{ backgroundColor: theme.colors.light }}
+        onScroll={(event) => handleScroll(event)}
+        scrollEventThrottle={16}
+      >
+        <CoverWrapper>
+          <Cover
+            source={{ uri: data.cover }}
+            resizeMode='stretch'
+            resizeMethod='scale'
+          />
+        </CoverWrapper>
+        <Container>
+          <Title>{data.title}</Title>
+          <ContainerRender>
+            <RenderHTML
+              contentWidth={width}
+              source={{ html: text }}
+              tagsStyles={tagsStyles}
+            />
+          </ContainerRender>
+          {
+            imageGallery.length > 0 &&
+            <ContainerGallery>
+              <TitleGallery>Galeria de Imagens</TitleGallery>
+              <ImageGallery
+                images={imageGallery}
+              />
+            </ContainerGallery>
+          }
+          <Published>{`publicado em: ${day} às ${hour}`}</Published>
+          <Footer>
+            <ContainerAvatar>
+              <Avatar
+                source={{ uri: data.user.image }}
+              />
+              <Name>{data.user.name}</Name>
+            </ContainerAvatar>
+            {
+              !!data.user.about &&
+              <AboutWrapper>
+                <About>{data.user.about}</About>
+              </AboutWrapper>
+            }
+          </Footer>
+        </Container>
+      </ScrollView>
+    </>
   );
 }
--- src/screens/Post/styles.ts
+++ src/screens/Post/styles.ts
@@ -1,8 +1,88 @@
+import { Dimensions } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
-  flex: 1;
+  position: relative;
+  width: 100%;
+  top: -40px;
+  border-top-right-radius: 60px;
+  border-top-left-radius: 60px;
+  background-color: ${({ theme }) => theme.colors.light};
+`;
+
+export const Title = styled.Text`
+  font-size: ${RFValue(26)}px;
+  font-weight: bold;
+  text-align: center;
+  margin: 15px 25px 0;
+`;
+
+export const CoverWrapper = styled.View`
+  width: ${Dimensions.get('window').width}px;
   justify-content: center;
   align-items: center;
+`;
+
+export const Cover = styled.Image`
   width: 100%;
+  height: ${RFValue(300)}px;
+`;
+
+export const ContainerRender = styled.View`
+  padding: 30px 0 0;
+  margin-bottom: 20px;
+`;
+
+export const ContainerGallery = styled.View`
+  padding: 0 0 30px;
+`;
+
+export const TitleGallery = styled.Text`
+  text-align: center;
+  font-size: ${RFValue(26)}px;
+  font-weight: bold;
+  padding: 0 20px 15px;
+`;
+
+export const Published = styled.Text`
+  text-align: center;
+  font-size: ${RFValue(12)}px;
+`;
+
+export const Footer = styled.View`
+  background-color: ${({ theme }) => theme.colors.text_light};
+  margin: 20px 20px 0;
+  padding-bottom: 5px;
+  border-radius: 40px;
+`;
+
+export const ContainerAvatar = styled.View`
+  flex-direction: row;
+  margin-bottom: 10px;
+  margin: 20px 20px 15px ;
+`;
+
+export const Avatar = styled.Image`
+  width: 60px;
+  height: 60px;
+  border-radius: 25px;
+  margin-right: 10px;
+`;
+
+export const Name = styled.Text`
+  font-size: ${RFValue(14)}px;
+  align-self: center;
+`;
+
+export const AboutWrapper = styled.View`
+  margin: 0 10px 20px;
+  padding: 10px 15px;
+  border-radius: 10px;
+  background-color: ${({ theme}) => theme.colors.light };
+
+`;
+
+export const About = styled.Text`
+  font-size: ${RFValue(14)}px;  
 `;
