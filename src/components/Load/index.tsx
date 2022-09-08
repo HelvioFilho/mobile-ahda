11d6ae6ddf759c5f9c2ace287496137edc3146c1
Hélvio Filho <helviosvf@gmail.com>
2022-09-08 16:07:34 -0300

    Style(Load): change color option

--- src/components/Load/index.tsx
+++ src/components/Load/index.tsx
@@ -4,13 +4,14 @@ import { useTheme } from 'styled-components';
 
 interface LoadProps {
   size: number;
+  player?: boolean;
 }
 
-export function Load({size}: LoadProps){
+export function Load({size, player=false}: LoadProps){
   const theme = useTheme();
   return (
     <ActivityIndicator 
-      color={theme.colors.tabBarColor.active}
+      color={theme.colors.tabBarColor[player ? 'player':'active']}
       size={size}
       style={{ flex: 1 }}
     />
