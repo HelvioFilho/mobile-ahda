e6f046156bd792f9762d8acedceb333f41a701de
Hélvio Filho <helviosvf@gmail.com>
2022-09-08 16:13:31 -0300

    Feat(PlayButton): added controllers and skin to background player

--- src/components/PlayButton/index.tsx
+++ src/components/PlayButton/index.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import TrackPlayer, {
+  Capability,
   State,
   usePlaybackState,
 } from 'react-native-track-player';
@@ -9,20 +10,32 @@ import { useTheme } from 'styled-components';
 
 import { Load } from '../Load';
 import { Button, Container } from './styles';
+import { postStore } from '../../services/store';
 
 export function PlayButton(){
   const urlRadio = "https://s18.maxcast.com.br:8707/live";
   const [loading, setLoading] = useState(false);
-  const [isPlayerOn, setIsPlayerOn] = useState(false);
 
   const playbackState = usePlaybackState();
   const theme = useTheme();
+  const {statusPlayer, setStatusPlayer } = postStore();
 
   async function setupPlayer(){
     try{
       await TrackPlayer.setupPlayer({});
-      await TrackPlayer.add({url: urlRadio});
-      setIsPlayerOn(true);
+      await TrackPlayer.add({
+        url: urlRadio, 
+        artwork: require('../../assets/angel-cover.png'),
+        title: 'Rádio A Hora do Anjo',
+        artist: 'De segunda à sexta de 18h às 19h'
+      });
+      await TrackPlayer.updateOptions({
+        stopWithApp: true,
+        capabilities:[Capability.Play, Capability.Pause],
+        compactCapabilities: [Capability.Play, Capability.Pause],
+        notificationCapabilities: [Capability.Play, Capability.Pause],
+      });
+      setStatusPlayer(true);
     }catch(e){
       console.log(e);
     }
@@ -30,14 +43,19 @@ export function PlayButton(){
 
   async function togglePlayback(playbackState){
     const currentTrack = await TrackPlayer.getCurrentTrack();
-    
     if(currentTrack !== null){
       if( playbackState === State.Paused || 
           playbackState === State.Ready
         ){
-        setLoading(true);
-        await TrackPlayer.play();
-        setLoading(false);
+        try{
+          setLoading(true);
+          await TrackPlayer.play();
+
+        }catch(e){
+          console.log(e);
+        }finally{
+          setLoading(false);
+        }
       }else{
         await TrackPlayer.pause();
       }
@@ -45,7 +63,7 @@ export function PlayButton(){
   }
 
   useEffect(() => {
-    if(!isPlayerOn){
+    if(statusPlayer === false){
       setupPlayer();
     }
   },[]);
@@ -67,7 +85,7 @@ export function PlayButton(){
       >
         {
           loading ?
-          <Load size={28} /> :
+          <Load size={28} player={true} /> :
           playbackState === State.Playing ?
           <Stop size={32} color={theme.colors.light} /> :
           <Play size={32} color={theme.colors.light} />
