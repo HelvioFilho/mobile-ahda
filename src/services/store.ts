3f88f2f764a9ccf1e56b56517d993e310e71c651
Hélvio Filho <helviosvf@gmail.com>
2022-09-08 16:23:02 -0300

    Feat(store): player boot control

--- src/services/store.ts
+++ src/services/store.ts
@@ -4,9 +4,13 @@ import { PostProps } from '../screens/Home';
 interface ZustandProps {
   data: PostProps;
   setData: (data: PostProps) => void;
+  statusPlayer: boolean;
+  setStatusPlayer: (statusPlayer: boolean) => void;
 }
 
 export const postStore = create<ZustandProps>((set) => ({
   data: {} as PostProps,
   setData: (data) => set({data}),
+  statusPlayer: false,
+  setStatusPlayer: (statusPlayer) => set({statusPlayer}),
 }));
