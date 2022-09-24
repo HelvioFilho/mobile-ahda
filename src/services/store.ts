35be56285c0394bf4e37d77dab210d75d8d3d8cf
Hélvio Filho <helviosvf@gmail.com>
2022-09-24 17:28:32 -0300

    Feat(store): added initial settings in zustand

--- src/services/store.ts
+++ src/services/store.ts
@@ -1,4 +1,5 @@
 import create from 'zustand';
+import { SettingsProps } from '../../App';
 import { PostProps } from '../screens/Home';
 
 interface ZustandProps {
@@ -6,11 +7,15 @@ interface ZustandProps {
   setData: (data: PostProps) => void;
   statusPlayer: boolean;
   setStatusPlayer: (statusPlayer: boolean) => void;
+  startSettings: SettingsProps;
+  setStartSettings: (startSettings: SettingsProps) => void;
 }
 
-export const postStore = create<ZustandProps>((set) => ({
+export const appDataStore = create<ZustandProps>((set) => ({
   data: {} as PostProps,
   setData: (data) => set({data}),
   statusPlayer: false,
   setStatusPlayer: (statusPlayer) => set({statusPlayer}),
+  startSettings: {} as SettingsProps,
+  setStartSettings: (startSettings) => set({startSettings}),
 }));
