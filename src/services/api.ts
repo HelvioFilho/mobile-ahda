091cbb57689c320fff03e529b8d80f5600c7e907
Hélvio Filho <helviosvf@gmail.com>
2022-10-07 02:20:07 -0300

    Chore: added bible api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,14 @@
 import axios from 'axios';
 
 const { BASE_URL } = process.env;
+const bibleUrl = "https://www.abibliadigital.com.br/api"; 
 
 const api = axios.create({
   baseURL: BASE_URL
 });
 
-export { api }
+const bible = axios.create({
+  baseURL: bibleUrl
+});
+
+export { api, bible }
