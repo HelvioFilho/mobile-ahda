47ac52a99b99510c0ef8f9a164d66eb27a7e7a33
Hélvio Filho <helviosvf@gmail.com>
2023-02-08 16:19:43 -0300

    Chore: installed and configured styled component

---
+++ src/theme/index.ts
@@ -0,0 +1,31 @@
+export default {
+  colors: {
+    text: '#0F0F0F',
+    text_mid: '#3E3E3E',
+    text_light: '#CFCFCF',
+
+    light: '#FFFFFF',
+    dark: '#000000',
+
+    background: '#F1F1F1',
+    backgroundModal: 'rgba(0,0,0,0.8)',
+    mark:'#F64F64',
+    splash: '#CAEAF5',
+
+    tabBarColor: {
+      active:'#4B92D4',
+      player: '#FFFFFF',
+      shadow: '#7F5DF0',
+    },
+    
+    success: '#4DCFC0',
+    warning: '#EEA63A',
+    error: '#E83F5B',
+  },
+  
+  fonts: {
+    Regular: 'Roboto_400Regular',
+    Medium: 'Roboto_500Medium',
+    Bold: 'Roboto_700Bold',
+  },
+}
