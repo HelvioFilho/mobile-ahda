027777bc3676300262f130f6765e15adf0472bec
Hélvio Filho <helviosvf@gmail.com>
2023-02-10 16:46:20 -0300

    Chore: created tag style

---
+++ src/utils/tagStyle.ts
@@ -0,0 +1,40 @@
+import { RFValue } from 'react-native-responsive-fontsize';
+
+export const getTagStyle = {
+  h1: {
+    margin: '5px 20px',
+  },
+  h2: {
+    margin: '0 20px',
+  },
+  h3: {
+    margin: '5px 20px',
+  },
+  h4: {
+    margin: '5px 20px',
+  },
+  h5: {
+    margin: '5px 20px',
+  },
+  h6: {
+    margin: '5px 20px',
+  },
+  img: {
+    margin: '0',
+  },
+  strong: {
+    margin: '0',
+  },
+  p: {
+    margin: '0 20px',
+    fontSize: `${RFValue(15)}px`
+  },
+  ul: {
+    margin: '10px 20px',
+    fontSize: `${RFValue(15)}px`
+  },
+  ol: {
+    margin: '10px 20px',
+    fontSize: `${RFValue(15)}px`
+  }
+}
